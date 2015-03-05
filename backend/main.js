8294f3e26138c2ef8e5ddf796ed1d1bc57a44a76
Aaron <ajblock@gmail.com>
2015-03-04 20:52:40 -0500

    Open, Save as working with hotkeys.

--- backend/main.js
+++ backend/main.js
@@ -1,56 +1,34 @@
 var gui = require('nw.gui'),
 	fs = require('fs'),
 	fdialogs = require('node-webkit-fdialogs');
+var saveDialog = new fdialogs.FDialog({
+	type: 'save', 
+	accept: ['.sw']
+}),
+	openDialog = new fdialogs.FDialog({
+		type: 'open',
+		accept: ['.sw']
+});
 
 $(function(){
-	var win = gui.Window.get();
-	var menubar = new gui.Menu({ type: 'menubar' });
-	win.showDevTools();
-	console.log(win);
-	menubar.createMacBuiltin('Screenwriter');
-	var file = new gui.MenuItem({ label: 'File' });
+	menu();
 
-	var fileMenu = new gui.Menu({ type: 'contextmenu' });
-	file.submenu = fileMenu;
-	fileMenu.append(new gui.MenuItem({ label: 'New' }));
-	fileMenu.append(new gui.MenuItem({ label: 'Save as...' }));
-	fileMenu.append(new gui.MenuItem({ label: 'Save' }));
-
-	menubar.insert(file, 1);
-	win.menu = menubar;
-	console.log(menubar.items);
-
-	var saveDialog = new fdialogs.FDialog({
-		type: 'save', 
-		accept: ['.sw']
-	}),
-		openDialog = new fdialogs.FDialog({
-			type: 'open',
-			accept: ['.sw']
-	});
-
-	$('#save_button').click(function(){
-		var content = getUnsavedContent()
-		saveDialog.saveFile(content, function(err, path){
-			if (err) {
-				console.log(err);
-			}
-		});
-	});
-	$("#open_button").click(function(){
-		openDialog.readFile(function(err, data, path){
-			$("textarea").text(data);
-		});
-	});
 });
 function saveAs() {
-
+	var content = getUnsavedContent();
+	saveDialog.saveFile(content, function(err, path){
+		if (err) {
+			console.log(err);
+		}
+	});
 }
 function save(file, savePath) {
 }
 
-function open(fileWithPath) {
-
+function open() {
+	openDialog.readFile(function(err, data, path){
+		$("textarea").text(data);
+	});
 }
 
 function getUnsavedContent() {
@@ -58,4 +36,40 @@ function getUnsavedContent() {
 	content = $('textarea').val();
 	contentBuffer = new Buffer(content, 'utf-8');
 	return contentBuffer;
+}
+
+function menu(){
+	var win = gui.Window.get();
+	var menubar = new gui.Menu({ type: 'menubar' });
+	win.showDevTools();
+	menubar.createMacBuiltin('Screenwriter');
+	var file = new gui.MenuItem({ label: 'File' });
+
+	var fileMenu = new gui.Menu({ type: 'contextmenu' });
+	file.submenu = fileMenu;
+	fileMenu.append(new gui.MenuItem({ label: 'New',
+										key: 'n',
+										modifiers: 'cmd' 
+									}));
+	fileMenu.append(new gui.MenuItem({ label: 'Save as...',
+									   key: 's',
+									   modifiers: 'cmd shift',
+									   click: function(){
+									   	saveAs();  
+									   }
+									}));
+	fileMenu.append(new gui.MenuItem({ label: 'Save',
+										key: 's',
+										modifiers: 'cmd'
+									}));
+	fileMenu.append(new gui.MenuItem({ label: 'Open',
+										key: 'o',
+										modifiers: 'cmd',
+										click: function() {
+											open();
+										}
+									}));
+
+	menubar.insert(file, 1);
+	win.menu = menubar;
 }
