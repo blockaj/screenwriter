4e04962e4579fdbf09dbb857786bc079ed750e75
Aaron <ajblock@gmail.com>
2015-03-26 13:39:43 -0400

    Able to find current line in text

--- backend/format.js
+++ backend/format.js
@@ -1,33 +1,38 @@
 function formatText() {
+	var index = 0;
 	var lineFormat;
 	var innerPage = $('.inner-page');
-	var selStart = '<p class="scene-heading"></p>'.indexOf('>');
-
 	$('.parenthetical').prepend('(');
 	$('.parenthetical').append(')');
 	if (!lineFormat) {
 		lineFormat = 'scene-heading';
 	}
+	innerPage.click(function(){
+		currentElement();
+	});
 	innerPage.keypress(function(e){
-		console.log('finding ');
 		if (e.keyCode == 13) {
 			e.preventDefault();
 			if (lineFormat == 'scene-heading') {
 				lineFormat = 'action';
 			} else if (lineFormat == 'action') {
-				if ($('.action').text() == '') {
+				if ($('.action').text() === '') {
 					lineFormat = 'scene-heading';
 				}
-				console.log(lineFormat);
 			} else if (lineFormat == 'character') {
-				lineFormat = 'speech';
+				console.log($('.character[data-index="' + index + '"]').text());
+				if ($('.character[data-index="' + index + '"]').text() === '') {
+					lineFormat = 'action';
+				} else {
+					console.log($('.character[data-index="' + index + '"]').text());
+					lineFormat = 'speech';
+				}
 			} else if (lineFormat == 'speech') {
 				lineFormat = 'character';
 			}
-			createNewElementWithFormat(lineFormat);
+			console.log(index);
+			index = createNewElementWithFormat(lineFormat, index);
 		}
-		console.log('Key pressed:', e.keyCode);
-
 	});
 	innerPage.keydown(function(e){
 		if (e.keyCode == 9) {
@@ -39,66 +44,84 @@ function formatText() {
 			if (lineFormat == 'action') {
 				lineFormat = 'character';
 			}
-			createNewElementWithFormat(lineFormat);
+			console.log(index);
+			index = createNewElementWithFormat(lineFormat, index);
 		}
 	});
-	innerPage.append('<p class="' + lineFormat + '"><br></p>');
+	innerPage.append('<p data-index=' + index + ' class="' + lineFormat + '"><br></p>');
 	console.log('<p class="' + lineFormat + '"><br></p>');
 }
-function createNewElementWithFormat(inputFormat) {
-	var innerPage = $('.innerPage');
-	innerPage.append('<p class="' + inputFormat + '"><br></p>');
-	var lineSelector = '.' + inputFormat;
-	var nodeContents = $(lineSelector).last()[0];
+function createNewElementWithFormat(inputFormat, dataIndex) {
+	var innerPage = $('.inner-page');
+	var prevTag = currentElement();
+	console.log(prevTag);
+	dataIndex++;
+	prevTag.after('<p data-index=' + dataIndex + ' class="' + inputFormat + '"><br></p>');
+	var nodeContents = innerPage.find('[data-index="' + dataIndex + '"]')[0];
 	var range = document.createRange();
 	range.selectNodeContents(nodeContents);
 	range.collapse(true);
 	var sel = window.getSelection();
 	sel.removeAllRanges();
 	sel.addRange(range);
+	currentElement();
+	return dataIndex;
 }
+
+function currentElement() {
+	var innerPage = $('inner-page');
+	var sel = window.getSelection();
+	console.log(sel);
+	var node = sel.anchorNode.parentNode;
+	console.log(node);
+	var jObject = $('[data-index="' + node.dataset.index + '"]');
+	return jObject;
+}
+
 function formatToJSON() {
 	var file = {};
 	file.scenes = [];
 	scenes = file.scenes;
 	var innerPage = $('.inner-page');
 	innerPage = innerPage[0].children;
+
+	//Loop through every p-tag in inner-page class
 	_.forEach(innerPage, function(p){
-		className = p.className;
-		if (className == 'scene-heading') {
-			var newItem = {
-				'type': 'sceneHeading',
-				'text': p.innerText
-			};
-			scenes.push(newItem);
-		} else if (className == 'action') {
-			var newItem = {
-				'type': 'action',
-				'text': p.innerText
-			};
-			scenes.push(newItem);
-		} else if (className == 'dialogue character') {
-			var newItem = {
-				'type': 'character',
-				'text': p.innerText
-			};
-			scenes.push(newItem);
-		} else if (className == 'dialogue parenthetical') {
-			var newItem = {
-				'type': 'parenthetical',
-				'text': p.innerText
-			};
-			scenes.push(newItem);
-		} else if (className == 'dialogue speech') {
-			var newItem = {
-				'type': 'speech',
-				'text': p.innerText
-			};
-			scenes.push(newItem);
+		var className = p.className;
+		var index = p.dataset.index;
+		var jsonClass;
+
+		//Reassigns name appropriate for JSON according to element class
+		switch(className) {
+			case 'scene-heading':
+				jsonClass = 'sceneHeading';
+				break;
+			case 'action':
+				jsonClass = 'action';
+				break;
+			case 'dialogue character':
+				jsonClass = 'character';
+				break;
+			case 'dialogue parenthetical':
+				jsonClass = 'parenthetical';
+				break;
+			case 'dialogue speech':
+				jsonClass = 'speech';
+				break;
 		}
+
+		//JSON object holding one line in the screenplay
+		var newItem = {
+			'type': jsonClass,
+			'text': p.innerText,
+			'index': index
+		};
+
+		scenes.push(newItem);
 	});
 	return file;
 }
+
 function formatToHTML(file) {
 	var characterList = [];
 	var htmlFile = "";
@@ -111,7 +134,7 @@ function formatToHTML(file) {
 	_.forEach(file.scenes, function(scene){
 		console.log(scene);
 		if(scene.type == 'sceneHeading') {
-			htmlFile = htmlFile.concat('<p class="scene-heading">' + scene.text + '</p>');
+			htmlFile = htmlFile.concat('<p class="scene-heading" data-index="' + scene.index + '">' + scene.text + '</p>');
 		} 
 		if (scene.type == 'action') {
 			htmlFile = htmlFile.concat('<p class="action">' + scene.text + '</p>');
@@ -134,6 +157,7 @@ function formatToHTML(file) {
 		if (scene.type == 'speech') {
 			htmlFile = htmlFile.concat('<p class="dialogue speech">' + scene.text + '</p>');
 		}
+
 	});
 	return htmlFile;
 }
