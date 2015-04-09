7eba704c4eda5bf1680139a0e76e40fb5b04cd2b
Aaron <ajblock@gmail.com>
2015-04-09 14:15:20 -0400

    Made a lot of changes

--- backend/format.js
+++ backend/format.js
@@ -1,83 +1,3 @@
-function formatText() {
-	var index = 0;
-	var lineFormat;
-	var innerPage = $('.inner-page');
-	$('.parenthetical').prepend('(');
-	$('.parenthetical').append(')');
-	if (!lineFormat) {
-		lineFormat = 'scene-heading';
-	}
-	innerPage.click(function(){
-		currentElement();
-	});
-	innerPage.keypress(function(e){
-		if (e.keyCode == 13) {
-			e.preventDefault();
-			if (lineFormat == 'scene-heading') {
-				lineFormat = 'action';
-			} else if (lineFormat == 'action') {
-				if ($('.action').text() === '') {
-					lineFormat = 'scene-heading';
-				}
-			} else if (lineFormat == 'character') {
-				console.log($('.character[data-index="' + index + '"]').text());
-				if ($('.character[data-index="' + index + '"]').text() === '') {
-					lineFormat = 'action';
-				} else {
-					console.log($('.character[data-index="' + index + '"]').text());
-					lineFormat = 'speech';
-				}
-			} else if (lineFormat == 'speech') {
-				lineFormat = 'character';
-			}
-			console.log(index);
-			index = createNewElementWithFormat(lineFormat, index);
-		}
-	});
-	innerPage.keydown(function(e){
-		if (e.keyCode == 9) {
-			e.preventDefault();
-			console.log('Tab key');
-			if (lineFormat == 'speech') {
-				lineFormat = 'parenthetical';
-			}
-			if (lineFormat == 'action') {
-				lineFormat = 'character';
-			}
-			console.log(index);
-			index = createNewElementWithFormat(lineFormat, index);
-		}
-	});
-	innerPage.append('<p data-index=' + index + ' class="' + lineFormat + '"><br></p>');
-	console.log('<p class="' + lineFormat + '"><br></p>');
-}
-function createNewElementWithFormat(inputFormat, dataIndex) {
-	var innerPage = $('.inner-page');
-	var prevTag = currentElement();
-	console.log(prevTag);
-	dataIndex++;
-	prevTag.after('<p data-index=' + dataIndex + ' class="' + inputFormat + '"><br></p>');
-	var nodeContents = innerPage.find('[data-index="' + dataIndex + '"]')[0];
-	var range = document.createRange();
-	range.selectNodeContents(nodeContents);
-	range.collapse(true);
-	var sel = window.getSelection();
-	sel.removeAllRanges();
-	sel.addRange(range);
-	currentElement();
-	return dataIndex;
-}
-
-function currentElement() {
-	var innerPage = $('inner-page');
-	var sel = window.getSelection();
-	console.log(sel);
-	var node = sel.anchorNode.parentNode;
-	console.log(node);
-	var jObject = $('[data-index="' + node.dataset.index + '"]');
-	return jObject;
-}
-
 function formatToJSON() {
 	var file = {};
 	file.scenes = [];
@@ -89,27 +9,7 @@ function formatToJSON() {
 	_.forEach(innerPage, function(p){
 		var className = p.className;
 		var index = p.dataset.index;
-		var jsonClass;
-
-		//Reassigns name appropriate for JSON according to element class
-		switch(className) {
-			case 'scene-heading':
-				jsonClass = 'sceneHeading';
-				break;
-			case 'action':
-				jsonClass = 'action';
-				break;
-			case 'dialogue character':
-				jsonClass = 'character';
-				break;
-			case 'dialogue parenthetical':
-				jsonClass = 'parenthetical';
-				break;
-			case 'dialogue speech':
-				jsonClass = 'speech';
-				break;
-		}
-
+		var jsonClass = htmlClassToJSON(className);
 		//JSON object holding one line in the screenplay
 		var newItem = {
 			'type': jsonClass,
@@ -157,11 +57,29 @@ function formatToHTML(file) {
 		if (scene.type == 'speech') {
 			htmlFile = htmlFile.concat('<p class="dialogue speech">' + scene.text + '</p>');
 		}
-
 	});
 	return htmlFile;
 }
-function formatInApp() {
+function htmlClassToJSON(htmlClass) {
+	var jsonClass;
 
-	return lineFormat;
+	//Reassigns name appropriate for JSON according to element class
+	switch(htmlClass) {
+		case 'scene-heading':
+			jsonClass = 'sceneHeading';
+			break;
+		case 'action':
+			jsonClass = 'action';
+			break;
+		case 'dialogue character':
+			jsonClass = 'character';
+			break;
+		case 'dialogue parenthetical':
+			jsonClass = 'parenthetical';
+			break;
+		case 'dialogue speech':
+			jsonClass = 'speech';
+			break;
+	}
+	return jsonClass;
 }
---
+++ backend/handler.js
@@ -0,0 +1,119 @@
+
+function formatText() {
+	var index = 0;
+
+	var lineFormat;
+
+	var innerPage = $('.inner-page');
+
+	$('.parenthetical').prepend('(');
+
+	$('.parenthetical').append(')');
+
+	if (!lineFormat) {
+		lineFormat = 'scene-heading';
+	}
+
+	innerPage.click(function(){
+		lineFormat = currentElement()[0].className;
+		console.log(lineFormat);
+	});
+
+	innerPage.keypress(function(e){
+
+		if (e.keyCode == 13) {
+
+			e.preventDefault();
+
+			if (lineFormat == 'scene-heading') {
+				lineFormat = 'action';
+			} 
+
+			else if (lineFormat == 'action') {
+				if ($('.action').text() === '') {
+					lineFormat = 'scene-heading';
+				}
+			} 
+
+			else if (lineFormat == 'character') {
+				console.log($('.character[data-index="' + index + '"]').text());
+				if ($('.character[data-index="' + index + '"]').text() === '') {
+					lineFormat = 'action';
+				} else {
+					console.log($('.character[data-index="' + index + '"]').text());
+					lineFormat = 'speech';
+				}
+			} 
+
+			else if (lineFormat == 'speech') {
+				lineFormat = 'character';
+			}
+			
+			index = createNewElementWithFormat(lineFormat, index);
+		}
+	});
+
+
+	innerPage.keydown(function(e){
+
+		if (e.keyCode == 9) {
+			e.preventDefault();
+			console.log('Tab key');
+
+			if (lineFormat == 'speech') {
+				lineFormat = 'parenthetical';
+			}
+
+			if (lineFormat == 'action') {
+				lineFormat = 'character';
+			}
+			convertElementToFormat('character');
+			//index = createNewElementWithFormat(lineFormat, index);
+		}
+	});
+
+	innerPage.append('<p data-index=' + index + ' class="' + lineFormat + '"><br></p>');
+
+	console.log('<p class="' + lineFormat + '"><br></p>');
+}
+function createNewElementWithFormat(inputFormat, dataIndex) {
+	var innerPage = $('.inner-page');
+	var prevTag = currentElement();
+	dataIndex++;
+	prevTag.after('<p data-index="' + dataIndex + '" class="' + inputFormat + '"><br></p>');
+	console.log('Appended tag with index: ' + dataIndex);
+	moveCursor(dataIndex);
+	return dataIndex;
+}
+function convertElementToFormat(inputFormat) {
+	var currentTag = currentElement();
+	var currentClass = currentTag.attr('class');
+	console.log(currentClass);
+	currentTag.removeClass(currentClass);
+	currentTag.addClass(inputFormat);
+}
+
+function moveCursor(index) {
+	console.log('Move cursor to index: ' + index);
+	var innerPage = $('.inner-page');
+	var nodeContents = innerPage.find('p[data-index="' + index + '"]')[0];
+	console.log(nodeContents);
+	var range = document.createRange();
+	range.selectNodeContents(nodeContents);
+	range.collapse(true);
+	var sel = window.getSelection();
+	sel.removeAllRanges();
+	sel.addRange(range);
+}
+
+function currentElement() {
+	var sel = window.getSelection();
+	var	node = sel.anchorNode;
+	if (node.textContent !== "") {
+		node = sel.anchorNode.parentElement;
+		console.log(node);
+	}
+	//Return jQuery object so it can be used with jQuery .after() 
+	var jObject = $('[data-index="' + node.dataset.index + '"]');
+	return jObject;
+}
