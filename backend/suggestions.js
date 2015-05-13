9bd39d3b3c72c7df9dafbfbf7c60d29de167eac4
Aaron <ajblock@gmail.com>
2015-05-13 15:57:57 -0400

    Chunky function well-commented

--- backend/suggestions.js
+++ backend/suggestions.js
@@ -5,43 +5,56 @@ function giveCharacterSuggestions(characterElement) {
 		'Alvin'
 	];
 
-
+	$('.inner-page').keypress(function() {
+		console.log(getSuggestions(currentElement(), availableTags));
+	});
 }
 
 
 
 function getSuggestions(el, availableTags) {
 	var suggestions = [];
-	console.log(currentElement());
-	$('.inner-page').keypress(function() {
-		var content = el.text();
-		console.log(content);
-		_.forEach(availableTags, function(tag) {
-			tag = tag.toLowerCase();
-			content = content.toLowerCase();
-			var suggest = tag.indexOf(content) != -1;
-			console.log(suggest);
-			if (suggest) {
-				var doesNotAlreadyExist = suggestions.indexOf(tag) == -1;
-				if (doesNotAlreadyExist) {
-					suggestions.push(tag);
-				}
-				console.log(suggestions);
-			} else {
-				var alreadyExists = suggestions.indexOf(tag) != -1;
-				if (alreadyExists) {
-					var index = suggestions.indexOf(tag);
-					suggestions.splice(index, 1);
-				}
+	var content = el.text();
+
+	_.forEach(availableTags, function(tag) {
+
+		//Suggestions should be case-insensitive
+		tag = tag.toLowerCase();
+		content = content.toLowerCase();
+
+		//Check if the suggestion is in the current string
+		var suggest = tag.indexOf(content) != -1;
+		if (suggest) {
+
+			//If it does not already exist in the array of suggestions
+			//add it to the array of suggestions 
+			var doesNotAlreadyExist = suggestions.indexOf(tag) == -1;
+			if (doesNotAlreadyExist) {
+				suggestions.push(tag);
 			}
-		});
+			console.log(suggestions);
+
+
+		} else {
+
+			//If a suggestion is no longer valid but is in the array
+			//of suggestions, remember to take that suggestion out
+			var alreadyExists = suggestions.indexOf(tag) != -1;
+			if (alreadyExists) {
+				var index = suggestions.indexOf(tag);
+				suggestions.splice(index, 1);
+			}
+		}
 	});
 }
 
 
-function allCharacters(doc) {
-	
-}
+
+function suggestionBox(suggestions) {
+	_.forEach(suggestions, function(suggestion){
+		
+	});
+}	
 
 
 function giveSceneHeadingSuggestions(shElement) {
